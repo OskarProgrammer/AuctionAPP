5d5c9da582f665204e0c2f1c82fe71ddf1221c5d
Oskar <Zaden>
2024-09-02 20:25:36 +0200

    Creating router provider with router, new routes: '/'

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
+//importing functions from react
+import { useEffect, useState } from 'react'
+
+//importing styles
 import './App.css'
+
+//importing bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+
+//importing layouts
+import {MainLayout} from "./Layouts/MainLayout/MainLayout"
+
+//creating routes
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<MainLayout/>}>
+
+  </Route>
+))
 
 function App() {
 
+  // useEffect(()=>{
+  //   const interval = setInterval(() => {
+  //     setCounter(counter+=1)
+  //   }, 1000);
+
+  //   return () => clearInterval(interval);
+  // })
+
   return (
     <>
-      
+      {/* setting router provider with router */}
+      <RouterProvider router={router}/>
     </>
   )
 }
