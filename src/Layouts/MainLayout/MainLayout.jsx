33c2d6bee6d9c31b2b1f0f4dbca1f8dc938faebc
Oskar <Zaden>
2024-09-02 20:25:12 +0200

    Creating MainLayout.jsx and MainLayout.css

---
+++ src/Layouts/MainLayout/MainLayout.jsx
@@ -0,0 +1,10 @@
+
+
+export const MainLayout = () => {
+
+    return (
+        <div className="container-fluid m-3">
+            <h1 className="display fw-bold text-center">Auction App</h1>
+        </div>
+    )
+}
