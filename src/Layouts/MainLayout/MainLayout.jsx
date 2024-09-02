55c21cc9cb482f2a78479c7a616e45582be31e76
Oskar <Zaden>
2024-09-02 22:24:27 +0200

    Creating container for the main content, also adding comments to the code

--- src/Layouts/MainLayout/MainLayout.jsx
+++ src/Layouts/MainLayout/MainLayout.jsx
@@ -1,14 +1,25 @@
 //importing components
 import { NavBar } from "../../components/NavBar/NavBar"
 
+//importing components and functions
+import { Outlet } from "react-router-dom"
+
 //Layout with all things , navbar, links, title
 export const MainLayout = () => {
 
     return (
         <div className="container-fluid">
+
+            {/* title of the page */}
             <h1 className="display-4 fw-bold text-center p-3">Auction App</h1>
             
+            {/* navbar */}
             <NavBar/>
+
+            {/* main content container */}
+            <div className="container-fluid my-3">
+                <Outlet/>
+            </div>
 
         </div>
     )
