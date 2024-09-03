6913322b97a843a5a0d8fd765c6957adedde345b
Oskar <Zaden>
2024-09-03 23:19:39 +0200

    Adding loader function auctionListLoader

--- src/pages/AuctionListPage/AuctionListPage.jsx
+++ src/pages/AuctionListPage/AuctionListPage.jsx
@@ -6,11 +6,15 @@ import { useEffect, useState } from "react"
 
 // importing api functions
 import { getCurrentUserInfo } from "../../api_functions/functions"
+import { useLoaderData } from "react-router-dom"
 
 
 export const AuctionListPage = () => {
+    // getting data from loader
+    const loaderData = useLoaderData()
+
     // initializing useState variable
-    let [currentUserData, setCurrentUserData] = useState({})
+    let [currentUserData, setCurrentUserData] = useState(loaderData)
 
     // creating useEffect with interval
     useEffect(()=>{
@@ -33,4 +37,12 @@ export const AuctionListPage = () => {
             <h1 className="display-4 fw-bold mb-5">List of your auctions</h1>
         </div>
     )
+}
+
+export const auctionListLoader = async () => {
+    // getting current user data
+    const currentUser = await getCurrentUserInfo()
+
+    // returning currentUser 
+    return currentUser
 }
