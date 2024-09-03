5c3dc2b0c0ecc15fe681a50768c1e4b545aec145
Oskar <Zaden>
2024-09-03 18:20:43 +0200

    Creating putRequest function

--- src/api_functions/functions.js
+++ src/api_functions/functions.js
@@ -50,3 +50,30 @@ export const postRequest = async (endpoint, payload) => {
 
     }
 }
+
+
+// PUT REQUESTS
+
+// function that put payload param into the endpoint param 
+export const putRequest = async ( endpoint , payload ) => {
+
+    // creating request options object to implement the body, headers and method of the request
+    const requestOptions = {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload)
+    }
+
+    //sending request
+    try {
+
+        // putting data to endpoint with given payload
+        await fetch ( endpoint , requestOptions)
+        
+    } catch {
+
+        //throwing error if occurs
+        throw new Error(`Error during putting data to ${endpoint} with payload ${payload}`)
+
+    }
+}
