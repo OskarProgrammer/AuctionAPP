d3272d96dc4e2f8c6b6682adcc045f3d70ed5808
Oskar <Zaden>
2024-09-03 18:27:36 +0200

    Creating loginAction in LoginPage.jsx

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,16 @@
 
 // importing functions and components from react library
-import { Form, Link } from "react-router-dom"
+import { Form, Link, redirect, useActionData } from "react-router-dom"
 
 //importing styles 
 import "./LoginPage.css"
+import { getRequest, putRequest } from "../../api_functions/functions"
 
 // login page in which user can log into the system
 export const LoginPage = () => {
+
+    // getting data from action if error occured
+    const actionData = useActionData()
 
 
     return (
@@ -16,7 +20,7 @@ export const LoginPage = () => {
             login => login
             pass => password
         */
-        <Form className=" mt-5 container-lg col-lg-5 col-12 bg-light d-flex flex-column gap-2 text-center p-3 shadow-lg rounded">
+        <Form method="POST" action="/login" className=" mt-5 container-lg col-lg-5 col-12 bg-light d-flex flex-column gap-2 text-center p-3 shadow-lg rounded">
         
 
             {/* header of the form*/}
@@ -34,6 +38,9 @@ export const LoginPage = () => {
                 {/* redirection if user havent got account */}
                 <p className="">Havent got account yet? <Link to="/register" className="text-dark linkParagraph">Click here</Link></p>
 
+                {/* displaying error message if exist */}
+                {actionData && actionData.error && <p className="text-danger fw-bold"> {actionData.error} </p>}
+
                 {/* button to submit the data */}
                 <button className="btn btn-outline-success col-6 mx-auto">Sign in</button>
                 
@@ -44,3 +51,52 @@ export const LoginPage = () => {
 
     )
 }
+
+// action function to route '/login' 
+export const loginAction = async ( {request}  ) => {
+
+    // getting data form
+    const data = await request.formData()
+
+    // getting login input
+    const login = data.get("login")
+
+    // getting password input 
+    const password = data.get("password")
+
+
+    // checking if login and password are not empty
+    if (login == "" || password == "") {
+
+        //returning error message 'All fields cannot be empty'
+        return { error : "All fields cannot be empty"}
+
+    }
+
+    // getting all users from endpoint 'http://localhost:3000/users/'
+    const users = await getRequest("http://localhost:3000/users/")
+
+    // initiazlizing object 'newCurrentUser'
+    let newCurrentUser = {}
+
+    // checking if login and password are valid
+    const i = users.findIndex(e => (e.login == login && e.password == password) )
+    if (i > -1){
+        newCurrentUser.id = users[i].id
+        newCurrentUser.isLogged = true
+    }else{
+        //returning error message 'Login or password is invalid'
+        return { error : "Login or password is invalid"}
+    }
+
+    // putting newCurrentUser into endpoint 'http://localhost:3000/currentUser/'
+    try {
+        await putRequest("http://localhost:3000/currentUser/", newCurrentUser)
+    } catch {
+        //returning error message 'Something went wrong during login process'
+        return { error : "Something went wrong during login process"}
+    }
+
+    //redirecting to '/' if everything was ok
+    return redirect("/")
+}   
