59d6147159ce886835ba6e45649663745e7d8f4b
Oskar <Zaden>
2024-09-03 22:54:18 +0200

    Creating sidebar in AccountLayout.jsx and also added some styles in AccountLayout.css

--- src/Layouts/AccountLayout/AccountLayout.jsx
+++ src/Layouts/AccountLayout/AccountLayout.jsx
@@ -2,17 +2,32 @@
 import "./AccountLayout.css"
 
 //importing functions and components from react library
-import { Outlet } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom"
 
 
 export const AccountLayout = () => {
 
 
     return (
-        <>
+        // whole layout 
+        <div className="container-fluid d-flex gap-2">
 
-            <Outlet/>
+            {/* left side of the layout */}
+            <div className="col-1 bg-light d-flex flex-column gap-3 p-3 shadow-lg border border-dark rounded sideBar">
+                
+                {/* balance money button */}
+                <NavLink to="" className="btn btn-outline-dark icon-30" >
+                    <i className="bi bi-bank"/>
+                </NavLink>
 
-        </>
+
+            </div>
+
+            {/* right side of the layout */}
+            <div className="col-11">
+                <Outlet />
+            </div>
+
+        </div>
     )
 }
