2f503cee6a094e9c1da2ffa874aca122dc3abe3f
Oskar <Zaden>
2024-09-04 12:13:09 +0200

    Creating form that add balance to account

--- src/pages/AccountBalancePage/AccountBalancePage.jsx
+++ src/pages/AccountBalancePage/AccountBalancePage.jsx
@@ -2,7 +2,7 @@
 import "./AccountBalancePage.css"
 
 // importing api functions
-import { getCurrentUserInfo } from "../../api_functions/functions"
+import { getCurrentUserInfo, getRequest, putRequest } from "../../api_functions/functions"
 
 // importing functions and components from react library
 import { useEffect, useState } from "react"
@@ -15,6 +15,16 @@ export const AccountBalancePage = () => {
 
     // initializing useState variable
     let [currentUserData, setCurrentUserData] = useState(loaderData)
+
+    // initializing useState variable isExpanded
+    let [isExpanded, setIsExpanded] = useState(false)
+
+    // initializing useState variable newBalance
+    let [newBalance, setNewBalance] = useState(0)
+
+    // initializing useState variable message
+    let [message, setMessage] = useState("")
+
 
     // creating useEffect with interval
     useEffect(()=>{
@@ -30,12 +40,64 @@ export const AccountBalancePage = () => {
         return () => { clearInterval(interval) }
     })
 
+    // function that add balance to the account
+    const addBalance = async ()=>{
+        // checking if value is positive
+        if (newBalance < 0) {
+            // setting form message
+            setMessage("Value has to be positive number")
+            
+            // hidding the form
+            setIsExpanded(false)
+
+            // setting newBalance to default 
+            setNewBalance(0)
+
+            return
+        }
+
+        // setting newBalnce of account
+        currentUserData.balance = parseInt(newBalance) + parseInt(currentUserData.balance)
+        
+        // setting useState variable newBalance to default
+        setNewBalance(0)
+
+        // putting new data
+        try {
+            await putRequest(`http://localhost:3000/users/${currentUserData.id}`, currentUserData)
+        } catch {
+            // throwing error if occurs
+            throw new Error("Error during updating balance of the account")
+        }
+
+        // hidding the form
+        setIsExpanded(false)
+
+        // setting form message
+        setMessage("Funds have been added")
+        
+    }
+
 
     // preview page of account balance
     return (
         <div className="container-fluid p-3 text-center">
             <h1 className="display-4 fw-bold mb-5">Account balance page</h1>
             <p className="fs-4">Current balance: {currentUserData.balance}</p>
+            {message != "" ? <p className="fs-4">{message}</p> : ""}
+            
+            {/* button to open form */}
+            <button className="btn btn-outline-primary" onClick={()=>{setIsExpanded(!isExpanded); setMessage("")}}>
+                {isExpanded ? "Close form" : "Add funds"}
+            </button>
+
+            {/* form of adding funds */}
+            {isExpanded ? <div className="container col-3 shadow-lg p-3 mt-3 d-flex flex-column gap-3">
+                <input className="p-2 text-center col-6 mx-auto" type="number" value={newBalance} onChange={(e)=>{setNewBalance(e.target.value)}}/>
+                <button className="btn btn-outline-success mx-auto col-6" onClick={()=>{ addBalance() } }>Add</button>
+            </div>
+            : ""}
+
         </div>
     )
 }
