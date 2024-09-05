a544f251e974ca9ac445a2fe87afd905a08b27f3
Oskar <Zaden>
2024-09-05 15:27:12 +0200

    Creating loaderMainPage, creating useEffect functions, creating rendering list of auctions with component <AuctionTab/>

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,13 +1,64 @@
-//importing styles
+// importing styles
 import "./MainPage.css"
+
+// importing api functions
+import { getAllAuctionsWithoutCurrentUser } from "../../api_functions/functions"
+
+// importing time functions
+import {  } from "../../date_functions/date_functions"
+
+// importing functions and components from react library
+import { useLoaderData } from "react-router-dom"
+import { useState , useEffect } from "react"
+
+// importing components
+import { AuctionTab } from "../../components/AuctionTab/AuctionTab"
 
 
 //main page look
 export const MainPage = () => {
 
+    // getting loaderData 
+    const loaderData = useLoaderData()
 
+    // creating useState variable auctions 
+    let [auctions, setAuctions] = useState(loaderData)
+
+    
+    // useEffect that get auctions every 3 seconds
+    useEffect(()=>{
+        const interval = setInterval( async () => {
+            // getting data from endpoint
+            const newAuctions = await getAllAuctionsWithoutCurrentUser()
+
+            // setting auctions useState variable
+            auctions = newAuctions
+            setAuctions(auctions)
+
+        }, 3000)
+
+        return () => { clearInterval(interval) }
+    }, [])
+
+
+    // preview page 
     return(
-        // preview data
-        <>Main Page</>
+        <div className="container-fluid text-center p-3">
+
+            {/* displaying auctions */}
+            {auctions.map((auction)=> (
+                //  tab
+                <AuctionTab auctionInfo={auction} />
+            ))}
+
+        </div>
     )
+}
+
+// loader of the mainPage
+export const loaderMainPage = async () =>{
+    // getting auctions which the owner is not current user
+    const auctions = await getAllAuctionsWithoutCurrentUser()
+
+    return auctions
 }
