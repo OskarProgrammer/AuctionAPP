d4dab377dbea8a39308f62952717c485069487ad
Oskar <Zaden>
2024-09-07 19:49:17 +0200

    Adding users container to MainPage

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,10 +2,7 @@
 import "./MainPage.css"
 
 // importing api functions
-import { getAllAuctionsWithoutCurrentUser } from "../../api_functions/functions"
-
-// importing time functions
-import {  } from "../../date_functions/date_functions"
+import { getAllAuctionsWithoutCurrentUser, getRequest } from "../../api_functions/functions"
 
 // importing functions and components from react library
 import { useLoaderData } from "react-router-dom"
@@ -13,16 +10,20 @@ import { useState , useEffect } from "react"
 
 // importing components
 import { AuctionTab } from "../../components/AuctionTab/AuctionTab"
+import { ProfileTab } from "../../components/ProfileTab/ProfileTab"
 
 
 //main page look
 export const MainPage = () => {
 
     // getting loaderData 
-    const loaderData = useLoaderData()
+    const [loaderAuctions, loaderUsers] = useLoaderData()
 
     // creating useState variable auctions 
-    let [auctions, setAuctions] = useState(loaderData)
+    let [auctions, setAuctions] = useState(loaderAuctions)
+
+    // creating useState variable users 
+    let [users, setUsers] = useState(loaderUsers)
 
     
     // useEffect that get auctions every one second
@@ -31,9 +32,16 @@ export const MainPage = () => {
             // getting data from endpoint
             const newAuctions = await getAllAuctionsWithoutCurrentUser()
 
+            // getting data from endpoint
+            const newUsers = await getRequest("http://localhost:3000/users/")
+
             // setting auctions useState variable
             auctions = newAuctions
             setAuctions(auctions)
+
+            // setting users useState variable
+            users = newUsers
+            setUsers(users)
 
         }, 1000)
 
@@ -44,6 +52,18 @@ export const MainPage = () => {
     // preview page 
     return(
         <div className="container-fluid text-center p-3">
+
+            {/* displaying users */}
+            <div className="container d-flex bg-dark text-light rounded usersContainer gap-3 p-3 border border-5 border-dark">
+                {/* header of users */}
+                <h1 className="my-auto fw-bold p-3">Users</h1>
+                
+                {/* mapping */}
+                {users.map((user) => (
+                    <ProfileTab userInfo={user} />
+                ))}
+
+            </div>
 
             {/* displaying auctions */}
             {auctions.map((auction)=> (
@@ -60,5 +80,8 @@ export const loaderMainPage = async () =>{
     // getting auctions which the owner is not current user
     const auctions = await getAllAuctionsWithoutCurrentUser()
 
-    return auctions
+    // getting users 
+    const users = await getRequest("http://localhost:3000/users/")
+
+    return [auctions,users]
 }
