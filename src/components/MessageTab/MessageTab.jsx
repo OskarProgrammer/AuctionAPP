5b29eebb892051cc51bc7400cad4b8b019aa8373
Oskar <Zaden>
2024-09-08 15:53:23 +0200

    Modifying message output

--- src/components/MessageTab/MessageTab.jsx
+++ src/components/MessageTab/MessageTab.jsx
@@ -2,12 +2,6 @@
 // importing styles
 import "./MessageTab.css"
 
-// importing functions and components from react library
-import { useEffect, useState } from "react"
-
-// importing api functions
-import { getRequest } from "../../api_functions/functions"
-
 export const MessageTab = (props) => {
 
     // getting message from props
@@ -16,28 +10,11 @@ export const MessageTab = (props) => {
     // getitng currentUser from props
     const currentUser = props.currentUser
 
-    // creating useState variable messageAuthorInfo
-    let [messageAuthorInfo, setMessageAuthorInfo] = useState({})
-
-
-    // useEffect to message author info
-    useEffect(()=>{
-        const timeout = setTimeout( async () => {
-            // getting message author info
-            messageAuthorInfo = await getRequest(`http://localhost:3000/users/${message.ownerID}`)
-
-            // setting messageAuthorInfo
-            setMessageAuthorInfo(messageAuthorInfo)
-        }, 1)
-
-        return () => { clearTimeout(timeout) }
-    })
-
     return (
         <>
             {/* message container */}
             { message.ownerID == currentUser.id ? <p className="fs-3 bg-primary text-light p-3 rounded">{message.message}</p> : 
-            <p className="fs-3 bg-dark text-light p-3 rounded">{messageAuthorInfo.login} : {message.message}</p>}
+            <p className="fs-3 bg-dark text-light p-3 rounded">{message.ownerName} : {message.message}</p>}
         </>
     )
 }
